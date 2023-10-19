14da3c3bf13f8a497bad68fd223c4078f81a4b49
AntoFígaro <antobfigaro@gmail.com>
2023-10-19 16:15:39 -0300

    inicio CRUD

--- AccessControl_1/app/src/main/java/com/example/accesscontrol_1/MainActivity.java
+++ AccessControl_1/app/src/main/java/com/example/accesscontrol_1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.accesscontrol_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.database.sqlite.SQLiteDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+//Abrimos la base de datos ‘DBUsuarios’ en modo escritura        
+        UsuariosSQLiteHelper usdbh = new UsuariosSQLiteHelper(this, "DBUsuarios", null, 1);
+        SQLiteDatabase db = usdbh.getWritableDatabase();
+//Si hemos abierto correctamente la base de datos
+        if(db != null)
+        {
+//Insertamos 5 usuarios de ejemplo
+            for(int i=1; i<=5; i++)
+            {
+//Generamos los datos
+                int codigo = i;
+                String nombre = "Usuario" + i;
+//Insertamos los datos en la tabla Usuarios
+                db.execSQL("INSERT INTO Usuarios (codigo, nombre) " +
+ "VALUES (" + codigo + ", '" + nombre +"')");
+            }
+//Cerramos la base de datos
+            db.close();
+} } }
